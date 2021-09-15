713c007652f8b67ea0bba6a09d375b2cef2645d5
Maksim Kolesnikov <maksim.kolesnikov@ivkhk.ee>
2021-09-15 13:02:46 +0300

    Тест приложения Корни V2

--- src/korni/korni.java
+++ src/korni/korni.java
@@ -1,52 +1,46 @@
-
 package korni;
-
 import java.util.Scanner;
 
 public class korni {
-
-public static double[] arvutus(double a, double b, double c) {
+public static double[] calc(double a, double b, double c) {
     double d = b * b - 4 * a * c;
+    System.out.println();
+    System.out.println("Дискриминант: " + d);
     if (d > 0) {
-            return new double[]//возвращает массив из 2-х зн-й
-            {(-b + Math.sqrt(d)) / 2 / a,
-            (-b - Math.sqrt(d)) / 2 / a};
+            return new double[]
+            {(-b + Math.sqrt(d)) / (2 * a),
+            (-b - Math.sqrt(d)) / (2 * a)};
     } 
     else 
         if (d == 0) {
-            return new double[] //возвращает массив из 1-го зн-я
-            {(-b / 2 / a),
-              (-b / 2 / a)};
-            
+            return new double[]
+            {(-b / (2 * a)),
+              (-b / (2 * a))};
     } 
         else {
-            return null; //возвращает пустое значение
-        }
+            return null;
+        }  
 }
-    
+
     public static void main(String[] args) {
-       Scanner scan=new Scanner(System.in);
-       String s;
-                System.out.println("Введите A ");
-          s=scan.next(); //ввод с клавиатуры открыт
-        double A=Double.parseDouble(s);  //string -> integer
-        
-        System.out.println("Введите B ");
-          s=scan.next(); //ввод с клавиатуры открыт
-       
-       double B=Double.parseDouble(s);  //string -> integer
-       
-      
-          System.out.println("Введите C ");
-          s=scan.next(); //ввод с клавиатуры открыт
-      
-       double C=Double.parseDouble(s);  //string -> integer
-    
-        double tulemus[] = arvutus(A,B,C);
-        if (tulemus!=null)
-        System.out.println("x1="+tulemus[0]+"  x2= "+tulemus[1]);
-        else
-            System.out.println("Корней нет ");
+        Scanner scan=new Scanner(System.in);
+        boolean running = true;
+        while(running){
+            String s;
+            System.out.println("Введите A ");
+            s=scan.next(); 
+            double A=Double.parseDouble(s);  //string -> integer
+            System.out.println("Введите B ");
+            s=scan.next();
+            double B=Double.parseDouble(s);  //string -> integer
+            System.out.println("Введите C ");
+            s=scan.next(); 
+            double C=Double.parseDouble(s);  //string -> integer
+            double[] result = calc(A,B,C);
+            if (result!=null)
+                System.out.println("Корни: x1="+result[0] +"; " + "x2= "+result[1]);
+            else
+                System.out.println("Корней нет ");
+            }
+        }
     }
-    
-}
--- test/korniTest.java
+++ test/korniTest.java
@@ -1,18 +1,18 @@
 import org.junit.Assert;
 import org.junit.Test;
-import static korni.korni.arvutus;
+import static korni.korni.calc;
 public class korniTest
 {
     @Test public void testNoSolutions() {
         Assert.assertNull(" нет решений ",
-                           arvutus(1, 1, 1)); }
+                           calc(1, 1, 1)); }
 
     @Test public void testOneSolution() {
         Assert.assertArrayEquals("одно решение,",
-                                new double[]{1}, //одно решение 
-                              arvutus(1, -2, 1), 0.001);}
+                                new double[]{1},
+                              calc(1, -2, 1), 0.001);}
 
     @Test public void testTwoSolutions() {
         Assert.assertArrayEquals("Два решения ",
-                                 new double[]{-1, 7}, //Два решения
-                                 arvutus(-1, 2, 3), 0.001);} }
+                                 new double[]{-1, 7},
+                                 calc(-1, 2, 3), 0.001);} }
